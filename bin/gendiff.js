43b57fc6f29113d5332c682cee0b21dae986f2ef
Vera Zhukova <mirvhytri@gmail.com>
2026-04-14 12:04:55 +0200

    Add step 2: modification gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,14 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2, options) => {
+    const format = options.format || 'stylish';
+    const result = genDiff(filepath1, filepath2, format);
+    console.log(result);
+  });
 
 program.parse(process.argv);
