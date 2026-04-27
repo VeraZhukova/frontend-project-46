db09d2d9c887ad750ade1c28a9d4039a20f2063a
Vera Zhukova <mirvhytri@gmail.com>
2026-04-27 12:44:04 +0200

    Add step 4: flat file comparison

--- src/genDiff.js
+++ src/genDiff.js
@@ -4,10 +4,26 @@ const genDiff = (filepath1, filepath2) => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
 
-  console.log(data1);
-  console.log(data2);
+  // получаем все ключи
+  const keys = [...Object.keys(data1), ...Object.keys(data2)];
 
-  return 'done';
+  // убираем дубликаты + сортируем
+  const uniqueKeys = [...new Set(keys)].sort();
+
+  const result = [];
+
+  for (const key of uniqueKeys) {
+    if (!(key in data2)) {
+      result.push(`- ${key}: ${data1[key]}`);
+    } else if (!(key in data1)) {
+      result.push(`+ ${key}: ${data2[key]}`);
+    } else if (data1[key] !== data2[key]) {
+      result.push(`- ${key}: ${data1[key]}`);
+      result.push(`+ ${key}: ${data2[key]}`);
+    }
+  }
+
+  return result.join('\n');
 };
 
 export default genDiff;
